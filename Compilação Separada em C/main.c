a376278f56e6bfc179e4519bc32b65b6e43911ad
othaviocc <othavioccorrea@gmail.com>
2025-11-11 21:12:10 -0300

    att_main

--- Compilação Separada em C/main.c
+++ Compilação Separada em C/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "soma.h"
 #include "subtrai.h"
+#include "mult.h"
+#include "div.h"
+#include "modulo.h"
+#include "potencia.h"
 
 int main(void)
 {
     int s;
     s = soma(4, 6);
+    printf("soma(4, 6) = %d\n", s);
     s = subtrai(s, 7);
-    printf("s = %d\n", s);
-
+    printf("subtrai(resultado, 7) = %d\n", s);
+    s = multiplica(3, 5);
+    printf("multiplica(3, 5) = %d\n", s);
+    s = divisao(10, 2);
+    printf("divisao(10, 2) = %d\n", s);
+    s = modulo(10, 3);
+    printf("modulo(10, 3) = %d\n", s);
+    s = potencia(2, 5);
+    printf("potencia(2, 5) = %d\n", s);
     return 0;
 }
